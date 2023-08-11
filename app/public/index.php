5188d727e923f609f56d9f956424fa0ace054596
marcoweb <marcocarvalho.web@gmail.com>
2023-08-11 20:02:16 +0000

    Configure App

--- app/public/index.php
+++ app/public/index.php
@@ -1,9 +1,10 @@
 <?php
-$_base_path = __DIR__ . '/';
-$_config_path = $_base_path . 'config/';
+// $_base_path = __DIR__ . '/';
+// $_config_path = $_base_path . 'config/';
 
-require $_base_path . 'includes/application.php';
+// require $_base_path . 'includes/application.php';
 
-echo $_base_path;
-<br />
-echo $_config_path;
+// echo $_base_path;
+// <br />
+// echo $_config_path;
+echo "Teste";
